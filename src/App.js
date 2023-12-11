ca953ece3aaa8bacdb3b081abd9b52ca017af236
Alexander Biryukov <74318440+AlexFromNorth@users.noreply.github.com>
2023-12-11 23:08:39 +0300

    created lesson 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
+import videoCards, { dec2bin } from "./cards.js";
+
+// найди самую дешевую видеокарту 4090ti и верни ее цену
+
+function searchCard() {
+
+  return videoCards[0].price;
+}
+let result = "undefined";
+
+eval(
+  atob(
+    "aWYoc2VhcmNoQ2FyZCgpPT0xMjUwKXtyZXN1bHQ9IlRydWUifWVsc2V7cmVzdWx0ID0gIkZhbHNlIn0="
+  )
+);
 
 function App() {
-  return (
-    <div className="App">
-
-test
-    </div>
-  );
+  return <div className="App">{result}</div>;
 }
 
 export default App;
