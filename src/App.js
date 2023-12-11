4f1cc2f150c5456cca679dbc554ad6e6c6db80c2
Alexander Biryukov <74318440+AlexFromNorth@users.noreply.github.com>
2023-12-11 21:15:26 +0300

    delete excess components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/headeer/Header";
-import Footer from "./components/footer/Footer";
-import News from "./components/news/News";
-import NavBar from "./components/navbar/NavBar";
-import Magazin from "./components/magazin/Magazin";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="container">
-        <News />
-        <NavBar />
-      </div>
-      <div className="magazin_wrapper">
-        <Magazin />
-        <Magazin />
-        <Magazin />
-      </div>
-      <Footer />
+
     </div>
   );
 }
